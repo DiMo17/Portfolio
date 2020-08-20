2b0390a39aa55fbefda5170d7e1e8efd2e1787fa
DiMo17 <45009193+DiMo17@users.noreply.github.com>
2020-08-20 22:44:14 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project__pagination',
+        bulletClass: 'project__bullet',
+        bulletActiveClass: 'project__bullet__active',
+        clickable: true
+    },
+});
